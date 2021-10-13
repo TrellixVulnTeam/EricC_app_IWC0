2cdc0abb207c6cd8bab3681b1d18e687e0416761
YeOldeEricC <eachapman26@gmail.com>
2021-10-14 00:56:18 +0100

    ...

--- EricC_website/views.py
+++ EricC_website/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse as response;
+from django.template import loader as ldr;
+from django.shortcuts import render as rnd;
 
-def index(request) :
-	return response('homepage');
+def home(request) :
+	# long way round
+	#tmp = ldr.get_template('home.html');
+	#return response(tmp.render({},request));
+	# shortcut way
+	return rnd(request,'home.html');
