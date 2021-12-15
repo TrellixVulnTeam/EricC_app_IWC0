bd703808fdba04f2f8680f773fe243c9cf9f3110
YeOldeEricC <eachapman26@gmail.com>
2021-12-15 01:16:13 +0000

    ...

--- EricC_website/urls.py
+++ EricC_website/urls.py
@@ -1,18 +1,3 @@
-"""EricC_website URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin;
 from django.urls import path;
 from django.views.generic.base import TemplateView as tmpv;
@@ -25,9 +10,9 @@ urlpatterns = [
     path('', pages.home, name='home'),
     path('about', pages.about, name='about'),
     path('contact', pages.contact, name='contact'),
-    # path('profile', pages.profile, name='profile'),
 
-    # auth
+    # profile stuff
+    # path('profile', pages.profile, name='profile'),
     # path('login', auth.LoginView.as_view(template_name='login.html'), name='login'),
     # path('logout', auth.LogoutView.as_view(), name='logout')
 ]
