14095d4256662cd18f239c63405453a17ebff5fd
YeOldeEricC <eachapman26@gmail.com>
2021-12-28 00:23:18 +0000

    input posting on log

--- EricC_website/pages/dbs/search.js
+++ EricC_website/pages/dbs/search.js
@@ -1,1 +1,8 @@
-console.log('lkagsdlhgsldhasdhgafksj')
+var field = '';
+
+function testF() {
+	console.log('before: ' + field);
+	var search_inp = document.getElementById("search_field").value;
+	field = search_inp;
+	console.log('after: ' + field);
+}
