a87c722b9bed52d0799b23780d5cf04b80ace49f
YeOldeEricC <eachapman26@gmail.com>
2022-02-03 20:34:38 +0000

    getting used to RN again, got dark screen up which auto resizes for web pages

--- EricC_RN/App.js
+++ EricC_RN/App.js
@@ -1,20 +1,65 @@
+import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions, Platform } from 'react-native';
+import { colour } from './Colours'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const H = Dimensions.get('window').height;
+const W = Dimensions.get('window').width;
+const OS = Platform.OS;
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			data: [],
+			SCREEN_W: W,
+			SCREEN_H: H,
+			PLATF_OS: OS,
+		}
+		this.styles = StyleSheet.create({
+			test: {
+
+			},
+		});
+		this.componentDidMount = this.componentDidMount.bind(this);
+		this.componentWillUnmount = this.componentWillUnmount.bind(this);
+		this.update_dims = this.update_dims.bind(this);
+		this.render_top_bar = this.render_top_bar.bind(this);
+	}
+
+	componentDidMount() {
+		this.interval = setInterval(() => this.update_dims(), 1000/60);
+	}
+	
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+	
+	// Specifically for web as user may resize the window
+	// so, not needed if an app as cannot resize.
+	update_dims() {
+		let NEW_H = Dimensions.get('window').height;
+		let NEW_W = Dimensions.get('window').width;
+		if (NEW_H != this.state.SCREEN_H || NEW_W != this.state.SCREEN_W) {
+			//console.log(this.state.SCREEN_H,this.state.SCREEN_W);
+			this.setState({SCREEN_W: NEW_W, SCREEN_H: NEW_H});
+		}
+	}
+
+	render_top_bar() {
+
+	}
+
+	render() {
+		return(
+			<View style={{
+				height: this.state.SCREEN_H,
+				width: this.state.SCREEN_W,
+				backgroundColor: colour.dark,
+			}}>
+			</View>
+		)
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
