c8f1c08f183cc78bbe76c57d52e096f71890b208
YeOldeEricC <eachapman26@gmail.com>
2022-02-06 15:26:56 +0000

    img/logo in header

--- EricC_RN/babel.config.js
+++ EricC_RN/babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+	api.cache(true);
+	return {
+		presets: [
+			'babel-preset-expo',
+			//'module:metro-react-native-babel-preset',
+		],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					extensions: ['.tsx','.ts','.js','.json'],
+				},
+			],
+			'react-native-reanimated/plugin',
+		],
+	};
 };
